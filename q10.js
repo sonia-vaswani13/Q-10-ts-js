ccec8bbff455b3533518d551ef61a41efb495f97
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 10:32:33 -0700

    Q 10TS/JS node js of 45 assignment
    Q 10TS/JS node js of 45 assignment

---
+++ q10.js
@@ -0,0 +1,14 @@
+//Adding Comments: Choose two of the programs you’ve written, 
+//and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple 
+//at this point, just add your name and the current date at the top of each program file. 
+//Then write one sentence describing what the program does.
+var a = 8;
+var b = 12;
+//additon
+console.log("".concat(a, "+").concat(b, "=").concat(a + b));
+//subtraction
+console.log("".concat(a, "-").concat(b, "=").concat(a - b));
+//Multiplication
+console.log("".concat(a, "*").concat(b, "=").concat(a * b));
+//Division
+console.log("".concat(a, "/ ").concat(b, "=").concat(a / b));
